d249edfb9e1d2dba54586a921968caba67b561b7
shahd.marouf <shahdjalal2018@gmail.com>
2025-07-23 09:19:08 +0300

    Add project files.

---
+++ Shahd_PresentationL/Controllers/CategoriesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shahd_DataAccessL.Data;
+using Shahd_DataAccessL.Models;
+
+namespace Shahd_PresentationL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoriesController(ApplicationDbContext context )
+        {
+            _context = context;
+        }
+    }
+}
