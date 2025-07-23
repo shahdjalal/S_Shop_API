338fd0f959c09bb24952193d380ee752585664b1
shahd.marouf <shahdjalal2018@gmail.com>
2025-07-23 09:19:08 +0300

    Add project files.

---
+++ DataAccessL/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shahd_DataAccessL.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Shahd_PresentationL/Program.cs
@@ -0,0 +1,38 @@
+
+using Microsoft.EntityFrameworkCore;
+using Shahd_DataAccessL.Data;
+
+namespace Shahd_PresentationL
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
